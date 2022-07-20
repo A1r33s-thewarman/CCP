3738f34ca1b38e1c286abb2d322d0bfac54f4fb2
ravindu <rkavishwara@gmail.com>
2022-07-20 16:31:37 +0530

    init

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, json
+from flask import request
+
+
+api = Flask(__name__)
+
+@api.route('/companies', methods=['POST'])
+def get_companies():
+    sentence = request.form.get('sentence')
+    ret = [{"id": 1, "sinhala": str(sentence)}]
+    return json.dumps(ret)
+
+if __name__ == '__main__':
+    api.run()
