2b194906f5ce10d5b4694c4461001e5535e05bd7
ravindu <rkavishwara@gmail.com>
2022-07-20 17:24:36 +0530

    import done

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from flask import Flask, json
 from flask import request
-
+import pathlib
+import random
+import string
+import re
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import TextVectorization
+import pandas as pd
+import numpy as np
+import pickle
 
 api = Flask(__name__)
 
 @api.route('/sentence', methods=['POST'])
 def get_sentence():
     sentence = request.form.get('sentence')
+
+    # do all things here
+
+
     ret = [{"id": 1, "sinhala": str(sentence)}]
     return json.dumps(ret)
 
